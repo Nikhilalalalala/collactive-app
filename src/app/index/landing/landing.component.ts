8887b1ddd959b008e9c2691d44cbdbd4a077b534
Nikhilalalalala <uvnikhi@gmail.com>
2022-01-24 23:00:37 +0800

    Trying to add to home page btn

--- src/app/index/landing/landing.component.ts
+++ src/app/index/landing/landing.component.ts
@@ -12,7 +12,19 @@ export class LandingComponent implements OnInit {
 
   promptEvent: any;
   showButton = false;
+  deferredPrompt: any;
 
+  @HostListener('window:beforeinstallprompt', ['$event'])
+  onbeforeinstallprompt(e) {
+    console.log(e);
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    this.deferredPrompt = e;
+    this.showButton = true;
+  }
+
+  
   constructor(
     private router: Router,
     private auth: AuthService,
@@ -20,17 +32,32 @@ export class LandingComponent implements OnInit {
     this.auth.getUserAuthState().authState.subscribe((user) => {
       if (user) { this.router.navigate(['/home']);}
     })
-    window.addEventListener('beforeinstallprompt', event => {
-      console.log(event)
-      this.promptEvent = event;
-    });
   }
+
 
   ngOnInit(): void {
   }
 
   onNextButtonClick() {
     this.router.navigate(['/login']);
+  }
+
+
+  addToHomeScreen() {
+    // hide our user interface that shows our A2HS button
+    this.showButton = false;
+    // Show the prompt
+    this.deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    this.deferredPrompt.userChoice
+      .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        this.deferredPrompt = null;
+      });
   }
 
   onAddToScreenButtonClick() {
